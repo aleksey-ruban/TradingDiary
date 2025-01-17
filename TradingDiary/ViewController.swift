6a1faec6ab58068e875e826e73587b4477cd3f80
aleksey-ruban <alekseyruban555@gmail.com>
2025-01-17 13:32:15 +0300

    Initial Commit

---
+++ TradingDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradingDiary
+//
+//  Created by Aleksey Ruban on 17.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TradingDiaryTests/TradingDiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  TradingDiaryTests.swift
+//  TradingDiaryTests
+//
+//  Created by Aleksey Ruban on 17.01.2025.
+//
+
+import Testing
+@testable import TradingDiary
+
+struct TradingDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
